00a3c1ac2bd2fdbd552ecdea80ed1ed423b59abc
onoue9 <filipeonouex@gmail.com>
2022-04-13 18:12:53 -0300

    <feat> requirement 11 complete: implementation of PVE class on PVE.ts

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,32 @@
+import { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  private _player1: SimpleFighter;
+  private _player2: SimpleFighter[];
+
+  constructor(player1: SimpleFighter, player2: SimpleFighter[]) {
+    super(player1, player2[0]);
+    this._player1 = player1;
+    this._player2 = player2;
+  }
+
+  fight() { 
+    console.log(this._player2);
+    this._player2.forEach(
+      (play2) => console.log(this._player1.attack(play2)),
+    );
+    this._player2.forEach(
+      (play2) => console.log(play2.attack(this._player1)),
+    );
+    if (this._player1.lifePoints < 0) { 
+      console.log('Player 1 morreu !');
+    }
+    this._player2.forEach(
+      (play2, index) => {
+        if (play2.lifePoints < 0) console.log(`Player2[${index + 1}] morreu !`);
+      },
+    );
+    return this._player1.lifePoints === -1 ? -1 : 1;
+  }
+}
